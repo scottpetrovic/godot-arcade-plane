99a124ca46ddcc469c203e25e336ca5af484836b
Scott Petrovic <scottpetrovic@gmail.com>
2024-08-17 11:19:01 -0500

    Adding a polish for the mission end screen. better text from "TextStudio"

--- MissionEndOverview/MissionEndOverview.gd
+++ MissionEndOverview/MissionEndOverview.gd
@@ -1,7 +1,11 @@
 extends Control
 
+@onready var repeat_texture: TextureRect = $RepeatTexture
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().create_timer(8.0).timeout
 	SceneTransition.change_scene("res://MainMenu/MainMenu.tscn")
+
+func _process(delta: float) -> void:
+	pass
