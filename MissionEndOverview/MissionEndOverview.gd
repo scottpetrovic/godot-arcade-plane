13a1fe125c968f90929bdfa6f89574ecfc065fee
Scott Petrovic <scottpetrovic@gmail.com>
2024-08-18 11:36:28 -0500

    Add more sounds and add global audio manager for background music

--- MissionEndOverview/MissionEndOverview.gd
+++ MissionEndOverview/MissionEndOverview.gd
@@ -3,12 +3,16 @@ extends Control
 @onready var repeat_texture: TextureRect = $RepeatTexture
 @onready var instructor_message: Label = $InstructorMessage
 
+var start_showing_text: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	instructor_message.visible_ratio = 0.0
-	await get_tree().create_timer(8.0).timeout
+	await get_tree().create_timer(2.0).timeout
+	start_showing_text = true
+	await get_tree().create_timer(10.0).timeout
 	SceneTransition.change_scene("res://MainMenu/MainMenu.tscn")
 
 func _process(delta: float) -> void:
-	instructor_message.visible_ratio += delta * 0.2
+	if start_showing_text:
+		instructor_message.visible_ratio += delta * 0.2
--- SceneTransition/scene_transition.gd
+++ SceneTransition/scene_transition.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
+@onready var global_sound_effect_player = $SoundEffectPlayer
+@onready var transition_sound: AudioStreamPlayer2D = $TransitionSound
+	
 func change_scene(target: String) -> void:
+	transition_sound.play()
 	$AnimationPlayer.play('dissolve')
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file(target)
