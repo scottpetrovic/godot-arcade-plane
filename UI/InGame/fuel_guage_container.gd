85cae74546b292e400c105fd7f401032b1fde9d1
Scott Petrovic <scottpetrovic@gmail.com>
2024-09-26 16:16:54 -0500

    Have fuel guage turn red and pulsate when low

--- UI/InGame/fuel_guage_container.gd
+++ UI/InGame/fuel_guage_container.gd
@@ -15,3 +15,19 @@ func _process(delta: float) -> void:
 	if player:
 		var remaining_fuel_percentage: float = player.current_fuel /  player.total_fuel
 		fuel_remaining_bar.size.x = full_fuel_size * remaining_fuel_percentage
+		
+		# turn fuel bar red when low on fuel
+		if remaining_fuel_percentage < 0.2:
+			oscillate_fuel_bar(true)
+			fuel_remaining_bar.color = 'b9404d' # red
+		else:
+			oscillate_fuel_bar(false)
+			fuel_remaining_bar.color = '00be89' # green
+
+func oscillate_fuel_bar(oscillate: bool) -> void:
+	if oscillate:
+		var scale_amount: float =  2.0 + sin(Time.get_ticks_msec()/500.0)
+		fuel_remaining_bar.scale.y = scale_amount
+		return
+		
+	fuel_remaining_bar.scale.y = 1.0
