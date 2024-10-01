f6fe3f49f9b0192a70c1a8218241725a7dec2274
Scott Petrovic <scottpetrovic@gmail.com>
2024-10-01 11:30:51 -0500

    Add scaling to water speed effect as we get closer to surface

--- Effects/Particles/WaterSpeedSplash/water_speed_splash.gd
+++ Effects/Particles/WaterSpeedSplash/water_speed_splash.gd
@@ -1,23 +1,33 @@
 extends Node3D
 
 var player: Node3D
+var start_splash_height: float = 3.0
+
+func _ready():
+	# get rid of this if we crashed
+	EventBus.player_crashed.connect(_on_player_crash)
+
+func _on_player_crash(location: String):
+	queue_free()
 
 func _process(delta: float) -> void:
-	
+
+	# make sure we have a player instance before continuing
 	if is_instance_valid(player) == false:
 		player = GameManager.get_player()
 		return
-	
+
 	update_splash_transform()
 	
 func update_splash_transform() -> void:
 
-	if player.global_position.y < 2:
+	if player.global_position.y < start_splash_height:
 		visible = true
 		var target_position = player.global_position
 		target_position.y = 0.1  # Keep it just above the water plane
 		global_position = target_position
 		calculate_splash_rotation() 
+		apply_sprite_scaling()
 	else:
 		visible = false
 
@@ -37,3 +47,17 @@ func calculate_splash_rotation() -> void:
 	# gives direction in radians between -180 and 180
 	var angle = atan2(player_forward.x, player_forward.z)
 	rotation.y = angle + (PI/2)
+
+func apply_sprite_scaling():
+	var min_y = 0  # Assuming water level is at y = 0
+	var max_y = start_splash_height  # The height at which scaling starts
+	var min_scale = 0.2
+	var max_scale = 5.0
+
+	var t = 1 - clamp((player.global_position.y - min_y) / (max_y - min_y), 0, 1)
+	var scale_factor = lerp(min_scale, max_scale, t)
+
+	# Apply scaling to all child sprites
+	for child in get_children():
+		if child is AnimatedSprite3D:
+			child.scale.y = scale_factor
