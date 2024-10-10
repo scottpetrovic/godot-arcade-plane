ea13cdedc219f649d48dbe74def092c452fde4ee
Scott Petrovic <scottpetrovic@gmail.com>
2024-10-10 16:16:08 -0500

    Update plane with better origin points. Animate airlon flap with input

--- Character/airplane_mesh.gd
+++ Character/airplane_mesh.gd
@@ -3,6 +3,9 @@ extends Node3D
 @export var plane: Player
 @export var animation_player: AnimationPlayer
 
+@export var flight_controller: FlightController
+
+
 var airplane_original_scale: float
 
 func open_landing_gears(value: bool) -> void:
@@ -11,11 +14,46 @@ func open_landing_gears(value: bool) -> void:
 	else:
 		animation_player.play_backwards("landing_gear_up")
 
+
 func _ready() -> void:
 	airplane_original_scale = scale.y
 	
 func _process(delta: float) -> void:
 	rotate_propellor(delta)
+	update_aileron(delta)
+	
+	
+func update_aileron(delta) -> void:
+
+	var pitch_input: float = flight_controller.pitch_input
+	var rotate_input: float = flight_controller.turn_input
+	var angle_multiplier: float = 0.6
+	
+	var left_airleron: MeshInstance3D = get_node("LeftRudder") as MeshInstance3D
+	var right_airleron: MeshInstance3D = get_node("RightRudder") as MeshInstance3D
+
+	# if we are turning, use that to update 3d model
+	if rotate_input != 0.0:
+		if rotate_input < 0.0: # roll right
+			left_airleron.rotation.x = rotate_input * angle_multiplier
+			right_airleron.rotation.x = -rotate_input * angle_multiplier
+		else: # roll left
+			left_airleron.rotation.x = rotate_input * angle_multiplier
+			right_airleron.rotation.x = -rotate_input * angle_multiplier
+		return
+
+	# only changing pitch
+	if pitch_input != 0.0:
+		left_airleron.rotation.x = -pitch_input * angle_multiplier
+		right_airleron.rotation.x = -pitch_input * angle_multiplier
+		return
+
+
+	# not using any input. straighten back out
+	left_airleron.rotation.x = 0
+	right_airleron.rotation.x = 0
+
+
 
 func rotate_propellor(delta: float) -> void:
 	# rotate propellor based off forward speed
