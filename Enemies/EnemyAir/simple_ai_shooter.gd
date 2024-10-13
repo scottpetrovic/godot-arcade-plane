7696da3f08533cd0d0e784f6048824ddb4487f03
Scott Petrovic <scottpetrovic@gmail.com>
2024-10-13 13:55:15 -0500

    Randomize AI shooter in case multiple instances appear at same time

--- Enemies/EnemyAir/simple_ai_shooter.gd
+++ Enemies/EnemyAir/simple_ai_shooter.gd
@@ -7,17 +7,24 @@ extends Node
 @export var Bullet = preload("res://Props/Bullet/Bullet.tscn")
 @export var attack_range = 25.0  # How close the enemy needs to be to attack (for future use)
 @export var bullet_spawn_point: Marker3D
+var rng = RandomNumberGenerator.new()
 
 var can_shoot = true
 
 func _ready():
+	rng.randomize()  # Initialize the random number generator
+	
 	shoot_timer.wait_time = shoot_delay
 	shoot_timer.timeout.connect(reset_can_shoot)
 
 func shoot():
 	if can_shoot:
 		can_shoot = false
-		shoot_timer.start()
+		
+		# add randomness so multiple instances won't shoot
+		# at same time
+		var random_delay = rng.randf_range(shoot_delay*0.6, shoot_delay*1.3)
+		shoot_timer.start(random_delay)
 
 		var bullet_instance: Area3D = Bullet.instantiate()
 		get_tree().root.add_child(bullet_instance)
